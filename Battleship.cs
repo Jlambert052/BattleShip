fd448787686a354becaff59fb46a0c2a29fc8bf0
Jack Lambert <jackwlambert14@gmail.com>
2023-03-03 23:58:52 -0500

    first additions

--- Battleship.cs
+++ Battleship.cs
@@ -1,5 +1,12 @@
 namespace Game {
     public class Battleship {
+        char water = '~';
+        char ship = 'S';
+        char hit = 'X';
+        char miss = 'O';
+        
+        static int[,] board = new int[8,8];
+
         public int BattleShip(int[,] field) {
             
         }
