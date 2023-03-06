21410ad7a0508f0f699b4f7964b3cf06b9fb93c5
Jack Lambert <jackwlambert14@gmail.com>
2023-03-06 00:39:03 -0500

    Game board made; visual created

--- Battleship.cs
+++ Battleship.cs
@@ -4,11 +4,36 @@ namespace Game {
         char ship = 'S';
         char hit = 'X';
         char miss = 'O';
-        
-        static int[,] board = new int[8,8];
 
-        public int BattleShip(int[,] field) {
-            
+        static int[,] ints = new int[8, 8];
+        static int[,] board = new int[8, 8];
+
+        public void  DisplayInts(int[,] ints) {
+            for(int i = 0; i < ints.GetLength(0); i++) {
+
+                for(int j = 0; j < ints.GetLength(1); j++) {
+                    Console.Write($"{ints[i, j]} | ");
+                }
+                System.Console.WriteLine();
+            }
         }
+
+        public void AssignBoard(char[,] board) {
+            for(int i = 0; i < board.GetLength(0); i++) {
+
+                for(int j = 0; j < board.GetLength(1); j++) {
+                    board[i,j] = water;
+                    System.Console.Write($"| {board[i,j]} ");
+                    if(j == 7) {
+                        System.Console.Write("|");
+                    }      
+                }
+                System.Console.WriteLine();
+            }
+        }
+
+        //public int BattleShip(int[,] field) {
+            
+        //}
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Game.Battleship Ship = new();
+
+int[,] ints = new int[8,5] { {1, 2, 3, 4, 5}, {2, 3, 4, 5, 6}, {3, 4, 5, 6, 7}, {4, 5, 6, 7, 8}, {5, 6, 7, 8, 9}, {6, 7, 8, 9, 10}, {7, 8, 9, 10, 11}, {8, 9, 10, 11, 12} };
+char[,] board = new char[8,8];
+
+//Ship.DisplayInts(ints);
+
+Ship.AssignBoard(board);
+
