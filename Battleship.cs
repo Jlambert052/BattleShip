7aeaf7bd2a0630cd3b14776037a5036c5cd55a12
Jack Lambert <jackwlambert14@gmail.com>
2023-03-10 15:51:33 -0500

    Board labeled and centered

--- Battleship.cs
+++ Battleship.cs
@@ -10,8 +10,11 @@ namespace Game {
         char[] cruiser = new char[] {'S','S', 'S', 'S'};
         char[] carrier = new char[] {'S','S', 'S', 'S', 'S'};
 
+        char[] columnNames = new char[] {'+', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+        char[] rowNames = new char[] {'+', '1', '2', '3', '4', '5', '6', '7', '8'};
+
         static int[,] ints = new int[8, 8];
-        static int[,] board = new int[8, 8];
+        static int[,] board = new int[9, 9];
 
         public void  DisplayInts(int[,] ints) {
             for(int i = 0; i < ints.GetLength(0); i++) {
@@ -29,11 +32,26 @@ namespace Game {
             for(int i = 0; i < board.GetLength(0); i++) {
 
                 for(int j = 0; j < board.GetLength(1); j++) {
-                    board[i,j] = water;
-                    System.Console.Write($"| {board[i,j]} ");
-                    if(j == 7) {
-                        System.Console.Write("|");
-                    }      
+                    if(i == 0) {
+                        board[i,j] = columnNames[j];
+                        if(i == 0 && j == 0) {}
+                        else
+                        System.Console.Write($"{columnNames[j], 4}");
+                        
+                    }if(j == 0) {
+                        board[i,j] = rowNames[i];
+                        System.Console.Write($" {rowNames[i], 5}");
+                    }if(i > 0 && j > 0) {
+
+                        board[i,j] = water;
+                        if(j == 1) {
+                        System.Console.Write($" |{board[i,j], 2} ");
+                        } else
+                        System.Console.Write($"|{board[i,j], 2} ");
+                        if(j == board.GetLength(0) - 1) {
+                            System.Console.Write("|");
+                        }      
+                    }
                 }
                 System.Console.WriteLine();
             }
@@ -48,7 +66,8 @@ namespace Game {
                 System.Console.WriteLine("That is not a valid ship! Re-enter your selection; 'tug', 'battleship', 'cruiser' or 'carrier'");
                 playerEntry = Console.ReadLine();
             }
-            System.Console.WriteLine("Where would you like to place this ship? Enter the first and last coordinate the ship occupies.");
+
+            System.Console.WriteLine("Where would you like to place this ship? Enter the first and last coordinate the ship occupies. Rows are A-H; Columns are 1-8. ");
 
 
 
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Welcome to BATTLESHIP. Place your ships well and fire at will
 Game.Battleship Ship = new();
 
 int[,] ints = new int[8,5] { {1, 2, 3, 4, 5}, {2, 3, 4, 5, 6}, {3, 4, 5, 6, 7}, {4, 5, 6, 7, 8}, {5, 6, 7, 8, 9}, {6, 7, 8, 9, 10}, {7, 8, 9, 10, 11}, {8, 9, 10, 11, 12} };
-char[,] board = new char[8,8];
+char[,] board = new char[9,9];
 
 //Ship.DisplayInts(ints);
 
